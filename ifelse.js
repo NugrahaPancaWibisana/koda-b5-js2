e7a67642d0c41cb99c11a3ad0a29d1287de02181
Nugraha Panca Wibisana <111733086+NugrahaPancaWibisana@users.noreply.github.com>
2025-11-05 15:14:03 +0700

    Delete ifelse.js

--- ifelse.js
+++
@@ -1,9 +0,0 @@
-let belanja = 1.5e6;
-
-if (belanja < 5e5) {
-  console.log(belanja);
-} else if (belanja >= 5e5 && belanja <= 1e6) {
-  console.log(belanja - 0.05 * belanja);
-} else if (belanja > 1e6) {
-  console.log(belanja - 0.1 * belanja);
-}
